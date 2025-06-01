2d6db498fb61b73ac733f560b3b8094bf48d2127
mubeel <muhammadmdgh1@gmail.com>
2025-06-01 14:13:15 +0300

    creating the list that containg a dictionary in arry of student information

--- script/script.js
+++ script/script.js
@@ -1,1 +1,23 @@
-console.log("yh i linked normalyvand im waiting for your comand");
+let student = [
+
+]
+
+function Submit(){
+    const name = document.querySelector('#name');
+    const quNum = document.querySelector('#qu-number');
+    const phone = document.querySelector('#phone-number');
+    const collage = document.querySelector('#collage');
+    const computerNumber = document.querySelector('#computer-num')
+
+
+    student.push({
+        name:name.value,
+        collage:collage.value,
+        phone:phone.value,
+        computerNumber:computerNumber.value,
+        logtime:"12/21pm",
+        exitTime:"21:34"
+    })
+    console.log(student);
+    
+}
