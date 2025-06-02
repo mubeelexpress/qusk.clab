67b0dc9808ea86bea8f242e1d7621fd009820e91
mubeel <muhammadmdgh1@gmail.com>
2025-06-02 12:41:43 +0300

    refrstion the code

--- script/script.js
+++ script/script.js
@@ -9,15 +9,26 @@ function Submit(){
     const collage = document.querySelector('#collage');
     const computerNumber = document.querySelector('#computer-num')
 
-
-    student.push({
+    if(name === '' || 
+        quNum === '' || 
+        phone === '' || 
+        collage === '' || 
+        computerNumber === '') {
+            name.style.outline = "red"
+        }else{
+        student.push({
         name:name.value,
+        quNum:quNum.value,
         collage:collage.value,
         phone:phone.value,
         computerNumber:computerNumber.value,
         logtime:"12/21pm",
         exitTime:"21:34"
+        
     })
+    
+        }
+
     console.log(student);
     
 }
